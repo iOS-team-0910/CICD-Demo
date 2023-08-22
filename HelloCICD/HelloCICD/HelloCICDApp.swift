072a7e3b788b11ac39d6a215561c8bf88b27ad01
HieuNH <hieunh0910@gmail.com>
2023-08-22 15:18:34 +0700

    create project demo

---
+++ HelloCICD/HelloCICD/HelloCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCICDApp.swift
+//  HelloCICD
+//
+//  Created by Tienvv on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloCICD/HelloCICD/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  HelloCICD
+//
+//  Created by Tienvv on 13/07/2023.
+//
+
+import Foundation
+
+class ViewModel {
+
+    var result: Int = 0
+
+    func sum(a: Int, b: Int) {
+        result = a + b
+    }
+}
